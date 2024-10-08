16b794808fb0f0f3a484a62924d0881bf1e63635
Michael J <30031129@cc.peralta.edu>
2024-10-07 21:26:27 -0700

    more code for sort

--- main/bubbleSort.cpp
+++ main/bubbleSort.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 
 namespace bubbleSort{
-void bubbleSort(int list[], int size){
-
-}
+    void bubbleSort(int list[], int size){
+        int wall = size - 1;
+        while(wall > 0){
+            for(int i = 0; i < wall; i++){
+                cout << "comparing " << list[i] << " to " << list[i + 1] << endl;
+                if(list[i] > list[i + 1]){
+                    cout << list[i] << " is larger. Swapping values " << endl;
+                    int temporaryHolder = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temporaryHolder;
+                }else{
+                cout << list[i] << " is smaller, no swap." << endl;
+                }
+            }
+        }
+    }
 }
